d9c1dfd5ce336f9427ea214450efb8c2f092519d
Martinch0 <martinsmihov@gmail.com>
2014-07-25 16:50:15 +0100

    Introduced input handling.

--- ThePuzzleGame/source/input.cpp
+++ ThePuzzleGame/source/input.cpp
@@ -2,37 +2,65 @@
 
 Input* g_Input = NULL;
 
-Input::Input()
+Input::Input() : m_Touched(false), m_PrevTouched(false)
 {
-
+	if (s3ePointerGetInt(S3E_POINTER_MULTI_TOUCH_AVAILABLE) != 0)
+	{
+		s3ePointerRegister(S3E_POINTER_TOUCH_EVENT, (s3eCallback)OnMultiTouchClickCallback, 0);
+		s3ePointerRegister(S3E_POINTER_TOUCH_MOTION_EVENT, (s3eCallback)OnMultiTouchDragCallback, 0);
+	}
+	else
+	{
+		s3ePointerRegister(S3E_POINTER_BUTTON_EVENT, (s3eCallback)OnClickCallback, 0);
+		s3ePointerRegister(S3E_POINTER_MOTION_EVENT, (s3eCallback)OnDragCallback, 0);
+	}
 }
 
 void Input::Update()
 {
-
+	s3ePointerUpdate();
 }
 
 void Input::Reset()
 {
+	m_PrevTouched = false;
+	m_Touched = false;
+}
 
+bool Input::isClick()
+{
+	return m_PrevTouched && !m_Touched;
+}
+
+bool Input::isDragging()
+{
+	return m_PrevTouched && m_Touched;
 }
 
 void Input::OnClickCallback(s3ePointerEvent* event)
 {
-
+	g_Input->m_PrevTouched = g_Input->m_Touched;
+	g_Input->m_Touched = event->m_Pressed != 0;
+	g_Input->m_x = event->m_x;
+	g_Input->m_y = event->m_y;
 }
 
 void Input::OnDragCallback(s3ePointerMotionEvent* event)
 {
-
+	g_Input->m_x = event->m_x;
+	g_Input->m_y = event->m_y;
 }
 
 void Input::OnMultiTouchClickCallback(s3ePointerTouchEvent* event)
 {
-
+	g_Input->m_PrevTouched = g_Input->m_Touched;
+	g_Input->m_Touched = event->m_Pressed != 0;
+	g_Input->m_x = event->m_x;
+	g_Input->m_y = event->m_y;
 }
 
 void Input::OnMultiTouchDragCallback(s3ePointerTouchMotionEvent* event)
 {
-
+	g_Input->m_x = event->m_x;
+	g_Input->m_y = event->m_y;
 }
--- ThePuzzleGame/source/input.h
+++ ThePuzzleGame/source/input.h
@@ -4,17 +4,41 @@
 #include "s3ePointer.h"
 
 class Input{
+private:
+	int m_x, m_y;
+	bool m_Touched;
+	bool m_PrevTouched;
 public:
 	Input();
 
+	/**
+	* @brief Updates the input. Should be called every frame.
+	*/
 	void Update();
+
+	/**
+	* @brief Resets the input.
+	*/
 	void Reset();
 
+	/**
+	* @brief Returns true when the current event has been a click.
+	*/
+	bool isClick();
+
+	/**
+	* @brief Returns true when the current event is a Drag.
+	*/
+	bool isDragging();
+
+	// Callback functions for Input handling
 	static void OnClickCallback(s3ePointerEvent* event);
 	static void OnDragCallback(s3ePointerMotionEvent* event);
 	static void OnMultiTouchClickCallback(s3ePointerTouchEvent* event);
 	static void OnMultiTouchDragCallback(s3ePointerTouchMotionEvent* event);
 };
 
+// a global variable for input handling
 extern Input* g_Input;
+
 #endif
