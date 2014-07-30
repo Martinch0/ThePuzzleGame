f00b3d25eaa9d5fd8b2eaa6dd9be9fae969ddea7
Martinch0 <martinsmihov@gmail.com>
2014-07-30 15:48:29 +0100

    Added comments to Puzzle.h Moved TODO list to Trello Added functionality to load a Puzzle from a String and to create a String from a Puzzle. Memory leaks fixed and code has been tidied up.

--- ThePuzzleGame/source/Puzzle.cpp
+++ ThePuzzleGame/source/Puzzle.cpp
@@ -2,64 +2,213 @@
 #include <stdlib.h>
 #include <iostream>
 
-Puzzle::Puzzle(int moves)
+void Puzzle::appendNumberToString(char *f, int n, const int max)
 {
-	this->field = new int[64];
-	this->fieldPrev = new int[64];
+	char a[8];
+	int i = 0;
+	do
+	{
+		a[i] = (n % 10) + '0';
+		i++;
+		n /= 10;
+	} while (i < max);
+	while (i > 0)
+	{
+		f[max - i] = a[i-1];
+		i--;
+	}
+}
+
+int Puzzle::readNumber(char **f, int max_length)
+{
+	int r = 0;
+	while (*f[0] >= '0' && *f[0] <= '9' && *f[0] != 0 && max_length > 0)
+	{
+		r *= 10;
+		r += *f[0] - '0';
+		(*f)++;
+		max_length--;
+	}
+	return r;
+}
+
+void Puzzle::skipNonDigits(char **f, int max_length)
+{
+	while ((*f[0] < '0' || *f[0] > '9') && *f[0] != 0 && max_length > 0)
+	{
+		(*f)++;
+		max_length--;
+	}
+}
+
+void Puzzle::readField(char **f)
+{
+	int pos = 0;
+	while (*f[0] >= '0' && *f[0] <= '9' && *f[0] != 0 && pos < this->size_X * this->size_Y)
+	{
+		this->field[pos] = this->fieldPrev[pos] = (*f)[0] - '0';
+		(*f)++;
+		pos++;
+	}
+}
+
+int Puzzle::getIndex(int x, int y)
+{
+	return y * this->size_X + x;
+}
+
+Puzzle::Puzzle(char *f)
+{
+	loadFromString(f);
+	this->s = new char[64];
+}
+
+Puzzle::Puzzle(int x, int y, int moves)
+{
+	initializeFieldArrays(x, y);
 	this->moves = moves;
-	s = new char[64];
+	this->s = new char[64];
 }
 
 Puzzle::~Puzzle()
 {
-	delete[] field;
-	delete[] fieldPrev;
+	if (this->size_X != 0 || this->size_Y)
+	{
+		delete[] field;
+		delete[] fieldPrev;
+	}
 	delete[] s;
 }
 
-void Puzzle::generate(int seed)
+void Puzzle::initializeFieldArrays(int x, int y)
 {
-	srand(seed);
-	for (int i = 0; i < 7; i++)
+	int sq = x * y;
+	int size = 2;
+	while (size < sq)
+		size *= 2;
+	if (this->size_X != 0 || this->size_Y != 0)
 	{
-		for (int j = 0; j < 7; j++)
-		{
-			this->field[i * 7 + j] = this->fieldPrev[i * 7 + j] = rand() % 4 + 1;
-		}
+		delete[] field;
+		delete[] fieldPrev;
 	}
-	field[24] = 0;
+	this->field = new int[size];
+	this->fieldPrev = new int[size];
+	this->size_X = x;
+	this->size_Y = y;
 }
 
-void Puzzle::printLine()
+void Puzzle::loadFromString(char *f)
 {
-	for (int i = 0; i < 7; i++)
+	// read the x size of the field
+	int x = readNumber(&f, MAX_PARSED_INT_LENGTH);
+	skipNonDigits(&f, SEPARATOR_LENGTH);
+
+	// read the y size of the field
+	int y = readNumber(&f, MAX_PARSED_INT_LENGTH);
+	skipNonDigits(&f, SEPARATOR_LENGTH);
+
+	// read the start x of the field
+	this->start_X = readNumber(&f, MAX_PARSED_INT_LENGTH);
+	skipNonDigits(&f, SEPARATOR_LENGTH);
+
+	// read the start y of the field
+	this->start_Y = readNumber(&f, MAX_PARSED_INT_LENGTH);
+	skipNonDigits(&f, SEPARATOR_LENGTH);
+
+	this->moves = readNumber(&f, MAX_PARSED_MOVES_LENGTH);
+	skipNonDigits(&f, SEPARATOR_LENGTH);
+
+	// initialize field arrays and load them from the string
+	initializeFieldArrays(x, y);
+	readField(&f);
+}
+
+void Puzzle::generateRandom(int seed)
+{
+	// generate a random field.
+	srand(seed);
+	for (int y = 0; y < this->size_Y; y++)
 	{
-		for (int j = 0; j < 7; j++)
+		for (int x = 0; x < this->size_X; x++)
 		{
-			std::cout << this->field[i * 7 + j];
+			this->field[this->getIndex(x, y)] = this->fieldPrev[this->getIndex(x, y)] = rand() % NUMBER_OF_ELEMENTS + 1;
 		}
 	}
+
+	// set the start X and Y to the middle of the field.
+	this->start_X = this->size_X / 2;
+	this->start_Y = this->size_Y / 2;
+	field[this->getIndex(this->start_X, this->start_Y)] = 0;
+}
+
+char* Puzzle::getAsString()
+{
+	char *r = new char[1 + MAX_PARSED_INT_LENGTH * 4 + 4 + MAX_PARSED_MOVES_LENGTH + 1 + this->size_X * this->size_Y];
+	int i = 0;
+
+	appendNumberToString(&r[i], this->size_X, MAX_PARSED_INT_LENGTH);
+	i += MAX_PARSED_INT_LENGTH;
+	r[i++] = '#';
+
+	appendNumberToString(&r[i], this->size_Y, MAX_PARSED_INT_LENGTH);
+	i += MAX_PARSED_INT_LENGTH;
+	r[i++] = '#';
+
+	appendNumberToString(&r[i], this->start_X, MAX_PARSED_INT_LENGTH);
+	i += MAX_PARSED_INT_LENGTH;
+	r[i++] = '#';
+
+	appendNumberToString(&r[i], this->start_Y, MAX_PARSED_INT_LENGTH);
+	i += MAX_PARSED_INT_LENGTH;
+	r[i++] = '#';
+
+	appendNumberToString(&r[i], this->moves, MAX_PARSED_MOVES_LENGTH);
+	i += MAX_PARSED_MOVES_LENGTH;
+	r[i++] = '#';
+
+	for (int y = 0; y < this->size_Y; y++)
+	{
+		for (int x = 0; x < this->size_X; x++)
+		{
+			r[i++] = this->field[this->getIndex(x, y)] + '0';
+		}
+	}
+
+	r[i] = 0;
+
+	return r;
 }
 
 void Puzzle::printAsMatrix()
 {
-	for (int i = 0; i < 7; i++)
+	for (int y = 0; y < this->size_Y; y++)
 	{
-		for (int j = 0; j < 7; j++)
+		for (int x = 0; x < this->size_X; x++)
 		{
-			std::cout << this->field[i * 7 + j] << ' ';
+			std::cout << this->field[this->getIndex(x, y)] << ' ';
 		}
 		std::cout << std::endl;
 	}
 }
 
+void Puzzle::printLine()
+{
+	for (int y = 0; y < this->size_Y; y++)
+	{
+		for (int x = 0; x < this->size_X; x++)
+		{
+			std::cout << this->field[this->getIndex(x, y)];
+		}
+	}
+}
+
 bool Puzzle::isSolved()
 {
-	for (int i = 0; i < 7; i++)
+	for (int y = 0; y < this->size_Y; y++)
 	{
-		for (int j = 0; j < 7; j++)
+		for (int x = 0; x < this->size_X; x++)
 		{
-			if (this->field[i * 7 + j] != 0)
+			if (this->field[this->getIndex(x, y)] != 0)
 				return false;
 		}
 	}
@@ -68,11 +217,11 @@ bool Puzzle::isSolved()
 
 bool Puzzle::isEqual(Puzzle* p)
 {
-	for (int i = 0; i < 7; i++)
+	for (int y = 0; y < this->size_Y; y++)
 	{
-		for (int j = 0; j < 7; j++)
+		for (int x = 0; x < this->size_X; x++)
 		{
-			if (this->field[i * 7 + j] != p->field[i * 7 + j])
+			if (this->field[this->getIndex(x, y)] != p->field[this->getIndex(x, y)])
 				return false;
 		}
 	}
@@ -81,39 +230,39 @@ bool Puzzle::isEqual(Puzzle* p)
 
 bool Puzzle::hasChanges()
 {
-	for (int i = 0; i < 7; i++)
+	for (int y = 0; y < this->size_Y; y++)
 	{
-		for (int j = 0; j < 7; j++)
+		for (int x = 0; x < this->size_X; x++)
 		{
-			if (this->field[i * 7 + j] != this->fieldPrev[i * 7 + j])
+			if (this->field[this->getIndex(x, y)] != this->fieldPrev[this->getIndex(x, y)])
 				return true;
 		}
 	}
 	return false;
 }
 
-int Puzzle::getElement(int i, int j)
+int Puzzle::getElement(int x, int y)
 {
-	return this->field[i * 7 + j];
+	return this->field[this->getIndex(x, y)];
 }
 
-void Puzzle::removeElement(int i, int j)
+void Puzzle::removeElement(int x, int y)
 {
-	this->field[i * 7 + j] = 0;
+	this->field[this->getIndex(x, y)] = 0;
 }
 
-void Puzzle::restoreElement(int i, int j, int el)
+void Puzzle::restoreElement(int x, int y, int el)
 {
-	this->field[i * 7 + j] = el;
+	this->field[this->getIndex(x, y)] = el;
 }
 
 void Puzzle::revert()
 {
-	for (int i = 0; i < 7; i++)
+	for (int y = 0; y < this->size_Y; y++)
 	{
-		for (int j = 0; j < 7; j++)
+		for (int x = 0; x < this->size_X; x++)
 		{
-			this->field[i * 7 + j] = this->fieldPrev[i * 7 + j];
+			this->field[this->getIndex(x, y)] = this->fieldPrev[this->getIndex(x, y)];
 		}
 	}
 }
@@ -123,33 +272,33 @@ void Puzzle::setField(int *f)
 	this->field = f;
 }
 
-void Puzzle::removeColor(int i, int j, int el, bool* visited)
+void Puzzle::removeColor(int x, int y, int el, bool* visited)
 {
 	if (this->isSolved())
 	{
 		return;
 	}
-	visited[i*7+j] = true;
-	if (this->getElement(i, j) == el)
+	visited[this->getIndex(x, y)] = true;
+	if (this->getElement(x, y) == el)
 	{
-		this->removeElement(i, j);
-		if (i + 1 < 7 && !visited[(i + 1) * 7 + j]) removeColor(i + 1, j, el, visited);
-		if (i - 1 >= 0 && !visited[(i - 1) * 7 + j]) removeColor(i - 1, j, el, visited);
-		if (j + 1 < 7 && !visited[i * 7 + j + 1]) removeColor(i, j + 1, el, visited);
-		if (j - 1 >= 0 && !visited[i * 7 + j - 1]) removeColor(i, j - 1, el, visited);
+		this->removeElement(x,y);
+		if (x + 1 < this->size_X && !visited[this->getIndex(x + 1,y)]) removeColor(x + 1, y, el, visited);
+		if (x - 1 >= 0 && !visited[this->getIndex(x - 1, y)]) removeColor(x - 1, y, el, visited);
+		if (y + 1 < this->size_Y && !visited[this->getIndex(x, y + 1)]) removeColor(x, y + 1, el, visited);
+		if (y - 1 >= 0 && !visited[this->getIndex(x, y - 1)]) removeColor(x, y - 1, el, visited);
 	}
-	else if (this->getElement(i, j) == 0)
+	else if (this->getElement(x,y) == 0)
 	{
-		if (i + 1 < 7 && !visited[(i + 1) * 7 + j]) removeColor(i + 1, j, el, visited);
-		if (i - 1 >= 0 && !visited[(i - 1) * 7 + j]) removeColor(i - 1, j, el, visited);
-		if (j + 1 < 7 && !visited[i * 7 + j + 1]) removeColor(i, j + 1, el, visited);
-		if (j - 1 >= 0 && !visited[i * 7 + j - 1]) removeColor(i, j - 1, el, visited);
+		if (x + 1 < 7 && !visited[this->getIndex(x + 1, y)]) removeColor(x + 1, y, el, visited);
+		if (x - 1 >= 0 && !visited[this->getIndex(x - 1, y)]) removeColor(x - 1, y, el, visited);
+		if (y + 1 < 7 && !visited[this->getIndex(x, y + 1)]) removeColor(x, y + 1, el, visited);
+		if (y - 1 >= 0 && !visited[this->getIndex(x, y - 1)]) removeColor(x, y - 1, el, visited);
 	}
 }
 
 Puzzle* Puzzle::createCopy(int moves, int currentMove)
 {
-	Puzzle *p = new Puzzle(moves);
+	Puzzle *p = new Puzzle(this->size_X, this->size_Y, moves);
 	for (int i = 0; i < 64; i++)
 	{
 		p->field[i] = p->fieldPrev[i] = this->field[i];
@@ -166,60 +315,26 @@ void Puzzle::findAllSolutions(int* solutions)
 	{
 		return;
 	}
-	bool v1[64] = { false };
-	bool v2[64] = { false };
-	bool v3[64] = { false };
-	bool v4[64] = { false };
-	Puzzle *p;
-	this->removeColor(3, 3, 1, v1);
-	if (this->isSolved())
+
+	Puzzle *p = NULL;
+	
+	for (int i = 1; i <= NUMBER_OF_ELEMENTS; i++)
 	{
-		solutions[moves]++;
-		this->s[this->cs] = '1';
-		std::cout << this->s << std::endl;
-	}
-	if (this->hasChanges())
-	{
-		p = this->createCopy(moves + 1, 1);
-		p->findAllSolutions(solutions);
-	}
-	this->revert();
-	this->removeColor(3, 3, 2, v2);
-	if (this->isSolved())
-	{
-		solutions[moves]++;
-		this->s[this->cs] = '2';
-		std::cout << this->s << std::endl;
-	}
-	if (this->hasChanges())
-	{
-		p = this->createCopy(moves + 1, 2);
-		p->findAllSolutions(solutions);
-	}
-	this->revert();
-	this->removeColor(3, 3, 3, v3);
-	if (this->isSolved())
-	{
-		solutions[moves]++;
-		this->s[this->cs] = '3';
-		std::cout << this->s << std::endl;
-	}
-	if (this->hasChanges())
-	{
-		p = this->createCopy(moves + 1, 3);
-		p->findAllSolutions(solutions);
-	}
-	this->revert();
-	this->removeColor(3, 3, 4, v4);
-	if (this->isSolved())
-	{
-		solutions[moves]++;
-		this->s[this->cs] = '4';
-		std::cout << this->s << std::endl;
-	}
-	if (this->hasChanges())
-	{
-		p = this->createCopy(moves + 1, 4);
-		p->findAllSolutions(solutions);
+		bool v[64] = { false };
+		this->removeColor(this->start_X, this->start_Y, i, v);
+		if (this->isSolved())
+		{
+			solutions[moves]++;
+			this->s[this->cs] = ('0' + i);
+			this->s[this->cs + 1] = 0;
+			std::cout << this->s << std::endl;
+		}
+		if (this->hasChanges())
+		{
+			p = this->createCopy(moves + 1, i);
+			p->findAllSolutions(solutions);
+			delete p;
+		}
+		this->revert();
 	}
 }
--- ThePuzzleGame/source/Puzzle.h
+++ ThePuzzleGame/source/Puzzle.h
@@ -1,26 +1,107 @@
+#define MAX_PARSED_INT_LENGTH 1
+#define MAX_PARSED_MOVES_LENGTH 2
+#define SEPARATOR_LENGTH 1
+#define NUMBER_OF_ELEMENTS 4
+
 class Puzzle
 {
 private:
+
+	// The current puzzle field.
 	int *field;
+
+	// The initial puzzle field.
 	int *fieldPrev;
+
+	// The size of the current field.
+	int size_X = 0, size_Y = 0;
+
+	// The start position of the Puzzle
+	int start_X = 0, start_Y = 0;
+
+	// The available moves left.
 	int moves = 0;
+
+	// A String containing all the moves made to complete a puzzle.
 	char *s;
+
+	// The index of the next free position in *s.
 	int cs = 0;
+
+	// Appends a number to a string.
+	void appendNumberToString(char *f, int n, const int max);
+
+	// Reads a number from a string until it reaches a non-digit symbol, the end of the string or has read more symbols than max_length.
+	int readNumber(char **f, int max_length);
+
+	// Skips any number of non-digit symbols until it reaches a number, the end of the string or has skipped more than max_length symbols.
+	void skipNonDigits(char **f, int max_length);
+
+	// Reads a field information from a string with the size of the initialized array.
+	void readField(char **f);
+
+	// Returns the index of element in position XxY
+	int getIndex(int x, int y);
 public:
-	Puzzle(int moves);
+
+	// Initialize a Puzzle from a string
+	Puzzle(char *f);
+
+	// Initialize an empty Puzzle, that has to be solved with the provided number of moves.
+	Puzzle(int x, int y, int moves);
+
+	// Destroy the created Puzzle
 	~Puzzle();
-	void generate(int seed);
+
+	// Initialize field arrays based on field size.
+	void initializeFieldArrays(int x, int y);
+
+	// Load a field from a string. The string should be in the following format:
+	// SIZE_X#SIZE_Y#START_X#START_Y#VALID_MOVES#FIELD
+	void loadFromString(char* f);
+
+	// Generate a random field using the provided random seed.
+	void generateRandom(int seed);
+
+	// Encode the current puzzle as a string.
+	char* getAsString();
+
+	// Print the field to the standard output as a matrix.
 	void printAsMatrix();
+
+	// Print the field to the standard output as a line.
 	void printLine();
+
+	// Checks if the Puzzle is solved.
 	bool isSolved();
+
+	// Checks if two puzzles are equal.
 	bool isEqual(Puzzle* p);
+
+	// Checks if there are changes to the current Puzzle, based on the previous field.
 	bool hasChanges();
-	int getElement(int i, int j);
-	void removeElement(int i, int j);
-	void restoreElement(int i, int j, int el);
+
+	// Returns the element at position XxY
+	int getElement(int x, int y);
+
+	// Removes the element at position XxY
+	void removeElement(int x, int y);
+
+	// Restores the element at position XxY to el.
+	void restoreElement(int x, int y, int el);
+
+	// Reverts all changes to the Puzzle.
 	void revert();
+
+	// Changes the field to the given array.
 	void setField(int *f);
-	void removeColor(int i, int j, int el, bool* visited);
+
+	// Removes all elements that are "visible" from position XxY and are from type el.
+	void removeColor(int x, int y, int el, bool* visited);
+
+	// Creates a copy of the current Puzzle.
 	Puzzle* createCopy(int moves, int currentMove);
+
+	// Generates all solutions to the current puzzle.
 	void findAllSolutions(int* solutions);
 };
