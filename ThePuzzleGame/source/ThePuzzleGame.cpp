42b8f700d45e3f16ba6f96f2887b22a8abce704d
Martinch0 <martinsmihov@gmail.com>
2014-07-31 12:26:34 +0100

    Fix the frame rate and make it easily manageable.

--- ThePuzzleGame/source/ThePuzzleGame.cpp
+++ ThePuzzleGame/source/ThePuzzleGame.cpp
@@ -5,6 +5,10 @@
 #include "scene.h"
 #include "mainMenu.h"
 
+#define FRAMES_PER_SECOND 30.0f
+#define FRAME_TIME (1.0f / FRAMES_PER_SECOND)
+#define FRAME_TIME_MS (FRAME_TIME * 1000.0f)
+
 
 // Main entry point for the application
 int main()
@@ -29,17 +33,22 @@ int main()
 	//Switch to the main menu
 	g_pSceneManager->SwitchTo(main_menu);
 
+	// Initialize variables for Frame Rate control
+	uint64 frame_start_time = 0;
+	int yield = 0;
 
     // Loop forever, until the user or the OS performs some action to quit the app
     while (!s3eDeviceCheckQuitRequest())
     {
+		// Record start time of frame.
+		frame_start_time = s3eTimerGetMs();
 
         //Update the input systems
         s3eKeyboardUpdate();
         s3ePointerUpdate();
 
 		//Update scene manager
-		g_pSceneManager->Update(0.003f); //NEED FRAME LIMIT
+		g_pSceneManager->Update(FRAME_TIME);
 
 		// Clear screen
 		Iw2DSurfaceClear(0xff000000);
@@ -50,8 +59,12 @@ int main()
 		// Show the drawing surface
 		Iw2DSurfaceShow();
 
-        // Sleep for 0ms to allow the OS to process events etc.
-        s3eDeviceYield(0);
+        // Calculate time to sleep for having a fixed frame rate.
+		yield = (int)(FRAME_TIME_MS - (s3eTimerGetMs() - frame_start_time));
+		if (yield < 0)
+			yield = 0;
+		// Sleep to allow the OS to process events etc.
+        s3eDeviceYield(yield);
     }
 
     //Terminate modules being used
