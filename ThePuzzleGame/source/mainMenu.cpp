cf8e018ae2e46ea97a32c4597ae5eb73e8293c67
Teodor Mihov <teodormitkovmihov@gmail.com>
2014-09-18 00:18:01 +0300

    The Main Menu should be scaling with resolution now. Made the button images a bit smaller.

--- ThePuzzleGame/source/mainMenu.cpp
+++ ThePuzzleGame/source/mainMenu.cpp
@@ -15,7 +15,9 @@ void MainMenu::Init()
 {
 	Scene::Init();
 
-	//Create menu background
+	float scale = IwGxGetScreenWidth() / ORIG_WIDTH;
+
+	// Create menu background
 	CSprite* background = new CSprite();
 	background->m_X = (float)IwGxGetScreenWidth() / 2;
 	background->m_Y = (float)IwGxGetScreenHeight() / 2;
@@ -24,36 +26,38 @@ void MainMenu::Init()
 	background->m_H = background->GetImage()->GetHeight();
 	background->m_AnchorX = 0.5;
 	background->m_AnchorY = 0.5;
-	//Fit background to screen size
+
+	// Fit background to screen size
 	background->m_ScaleX = (float)IwGxGetScreenWidth() / background->GetImage()->GetWidth();
 	background->m_ScaleX = (float)IwGxGetScreenHeight() / background->GetImage()->GetHeight();
 	AddChild(background);
 
-	//Create Start Game button
-	float y_pos = (float)IwGxGetScreenHeight() * 0.5f;
+	// Create Start Game button
+	float x_pos = IwGxGetScreenWidth() * 0.5f;
+	float y_pos = IwGxGetScreenHeight() * 0.5f;
 	playButton = new CSprite();
 	playButton->SetImage(g_pResources->getPlayButton());
-	playButton->m_X = IwGxGetScreenWidth() / 2.0f;
+	playButton->m_X = x_pos;
 	playButton->m_Y = y_pos;
 	playButton->m_W = playButton->GetImage()->GetWidth();
 	playButton->m_H = playButton->GetImage()->GetHeight();
 	playButton->m_AnchorX = 0.5f;
 	playButton->m_AnchorY = 0.5f;
-	playButton->m_ScaleX = 0.3f;
-	playButton->m_ScaleY = 0.3f;
+	playButton->m_ScaleX = scale;
+	playButton->m_ScaleY = scale;
 	AddChild(playButton);
 
-	//Create HowToPlay Button
+	// Create HowToPlay Button
 	howToPlayButton = new CSprite();
 	howToPlayButton->SetImage(g_pResources->getHowToPlayButton());
-	howToPlayButton->m_X = IwGxGetScreenWidth() / 2.0f;
-	howToPlayButton->m_Y = y_pos + (playButton->GetImage()->GetHeight() * playButton->m_ScaleY);
+	howToPlayButton->m_X = x_pos;
+	howToPlayButton->m_Y = y_pos + (playButton->GetImage()->GetHeight() * scale);
 	howToPlayButton->m_W = howToPlayButton->GetImage()->GetWidth();
 	howToPlayButton->m_H = howToPlayButton->GetImage()->GetHeight();
 	howToPlayButton->m_AnchorX = 0.5f;
 	howToPlayButton->m_AnchorY = 0.5f;
-	howToPlayButton->m_ScaleX = 0.3f;
-	howToPlayButton->m_ScaleY = 0.3f;
+	howToPlayButton->m_ScaleX = scale;
+	howToPlayButton->m_ScaleY = scale;
 	AddChild(howToPlayButton);
 }
 
@@ -66,19 +70,19 @@ void MainMenu::Update(float deltaTime, float alphaMul)
 
 	Scene::Update(deltaTime, alphaMul);
 
-	//Detect screen tap
+	// Detect screen tap
 	if (m_IsInputActive && m_Manager->GetCurrent() == this && g_Input->isClick())
 	{
 		g_Input->Reset();
 		if (playButton->HitTest(g_Input->GetX(), g_Input->GetY()))
 		{
-			//Switch to game scene
+			// Switch to game scene
 			g_pSceneManager->SwitchTo(g_pSceneManager->Find("field"));
 		}
 
 		if (howToPlayButton->HitTest(g_Input->GetX(), g_Input->GetY()))
 		{
-			//Switch to HowToPlay scene
+			// Switch to HowToPlay scene
 			g_pSceneManager->SwitchTo(g_pSceneManager->Find("howtoplay"));
 		}
 	}
